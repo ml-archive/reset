65cb140ed73238ce86f79ca4b5937bc803dec351
Siemen Sikkema <siemensikkema@users.noreply.github.com>
2018-06-11 13:43:04 +0200

    Clean up API docs (to trigger ci)

--- Sources/Reset/Command/GeneratePasswordResetTokenCommand.swift
+++ Sources/Reset/Command/GeneratePasswordResetTokenCommand.swift
@@ -29,8 +29,8 @@ public struct GeneratePasswordResetTokenCommand<U: PasswordResettable>: Command 
     /// ```
     ///
     /// - Parameters:
-    ///   - databaseIdentifier: identifier of database from where to load the user
-    ///   - makeFilter: used to create the filter from the query
+    ///   - databaseIdentifier: identifier of database from where to load the user.
+    ///   - makeFilter: used to create the filter from the query.
     public init(
         databaseIdentifier: DatabaseIdentifier<U.Database>,
         makeFilter: @escaping (String) throws -> ModelFilter<U>
@@ -64,7 +64,7 @@ public struct GeneratePasswordResetTokenCommand<U: PasswordResettable>: Command 
 extension GeneratePasswordResetTokenCommand where U.ID: LosslessStringConvertible {
     /// Creates a new password reset token command that looks up users by database identifier.
     ///
-    /// - Parameter databaseIdentifier: identifier of database from where to load the user
+    /// - Parameter databaseIdentifier: identifier of database from where to load the user.
     public init(
         databaseIdentifier: DatabaseIdentifier<U.Database>
     ) {
