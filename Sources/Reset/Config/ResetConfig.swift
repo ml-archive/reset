c591387d184b6719a86c359c653b84b0d4f9a31a
Siemen Sikkema <siemensikkema@users.noreply.github.com>
2018-09-03 15:51:23 +0200

    Use consistent capitalization of URL

--- Sources/Reset/Config/ResetConfig.swift
+++ Sources/Reset/Config/ResetConfig.swift
@@ -4,7 +4,7 @@ import Vapor
 
 public struct ResetConfig<U: JWTAuthenticatable & PasswordResettable>: Service {
     public let name: String
-    public let baseUrl: String
+    public let baseURL: String
     public let endpoints: ResetEndpoints
     public let shouldRegisterRoutes: Bool
     public let signer: ExpireableJWTSigner
@@ -14,21 +14,21 @@ public struct ResetConfig<U: JWTAuthenticatable & PasswordResettable>: Service {
     ///
     /// - Parameters:
     ///   - name: the name of your project.
-    ///   - baseUrl: the base url of your project. Used for generating reset password links.
+    ///   - baseURL: the base url of your project. Used for generating reset password links.
     ///   - endpoints: determines the endpoints for the routes
     ///   - shouldRegisterRoutes: determines whether to register the default routes at boot time.
     ///   - signer: signer and expiration period for password reset tokens.
     ///   - responses: contains the responses to be returned when requesting the endpoints.
     public init(
         name: String,
-        baseUrl: String,
+        baseURL: String,
         endpoints: ResetEndpoints = .default,
         shouldRegisterRoutes: Bool = true,
         signer: ExpireableJWTSigner,
         responses: ResetResponses<U> = .default
     ) {
         self.name = name
-        self.baseUrl = baseUrl
+        self.baseURL = baseURL
         self.signer = signer
         self.endpoints = endpoints
         self.shouldRegisterRoutes = shouldRegisterRoutes
@@ -47,7 +47,7 @@ public extension ResetConfig {
         let signer = try object.signer(for: context, on: req)
         return try object.signToken(using: signer, on: req)
             .flatMap(to: Void.self) { token in
-                let url = self.baseUrl
+                let url = self.baseURL
                     .appending("\(self.endpoints.resetPassword ?? "")/\(token)")
                 return try object.sendPasswordReset(
                     url: url,
