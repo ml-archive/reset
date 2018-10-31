402155deaa789caebe6a1a23812367d0fdc6403a
Steffen D. Sommer <steffendsommer@icloud.com>
2018-10-31 10:39:18 +0100

    Introduce controller, align with router guidelines

---
+++ Sources/Reset/Controllers/ResetController.swift
@@ -0,0 +1,87 @@
+import Authentication
+import JWT
+import Sugar
+import Vapor
+
+public final class ResetController<U: JWTAuthenticatable & PasswordResettable> {
+    public func renderResetPasswordRequestForm(req: Request) throws -> Future<Response> {
+        let config: ResetConfig<U> = try req.make()
+        return try config.responses.resetPasswordRequestForm(req)
+    }
+
+    public func resetPasswordRequest(req: Request) throws -> Future<Response> {
+        let config: ResetConfig<U> = try req.make()
+        return try U.RequestReset.create(on: req)
+            .flatMap(to: U?.self) { try U.find(by: $0, on: req) }
+            .flatTry { user -> Future<Void> in
+                guard let user = user else {
+                    // ignore case where user could not be found to prevent malicious attackers from
+                    // finding out which accounts are available on the system
+                    return .done(on: req)
+                }
+                return try config.reset(
+                    user,
+                    context: U.Context.requestResetPassword(),
+                    on: req
+                )
+            }
+            .flatMap(to: Response.self) { _ in
+                try config.responses.resetPasswordEmailSent(req)
+            }
+    }
+
+    public func renderResetPasswordForm(req: Request) throws -> Future<Response> {
+        let config: ResetConfig<U> = try req.make()
+        let payload = try config.extractVerifiedPayload(from: req.parameters.next())
+
+        return try U
+            .authenticate(using: payload, on: req)
+            .unwrap(or: ResetError.userNotFound)
+            .flatMap(to: Response.self) { user in
+                guard user.passwordChangeCount == payload.passwordChangeCount else {
+                    throw ResetError.tokenAlreadyUsed
+                }
+                return try config.responses.resetPasswordForm(req, user)
+        }
+    }
+
+    public func resetPassword(req: Request) throws -> Future<Response> {
+        let config: ResetConfig<U> = try req.make()
+        let payload = try config.extractVerifiedPayload(from: req.parameters.next())
+
+        return try U
+            .authenticate(using: payload, on: req)
+            .unwrap(or: ResetError.userNotFound)
+            .try { user in
+                guard user.passwordChangeCount == payload.passwordChangeCount else {
+                    throw ResetError.tokenAlreadyUsed
+                }
+            }
+            .flatMap(to: U.self) { user in
+                try U.ResetPassword.create(on: req)
+                    .flatMap(to: U.self) { resetPassword in
+                        var user = user
+                        let password = resetPassword[keyPath: U.ResetPassword.readablePasswordKey]
+                        user[keyPath: U.passwordKey] = try U.hashPassword(password)
+                        user.passwordChangeCount += 1
+                        return user.save(on: req)
+                    }
+            }
+            .flatMap(to: Response.self) { user in
+                try config.responses.resetPasswordSuccess(req, user)
+            }
+    }
+}
+
+fileprivate extension ResetConfig {
+    fileprivate func extractVerifiedPayload(from token: String) throws -> U.JWTPayload {
+        let payload = try JWT<U.JWTPayload>(
+            from: token.convertToData(),
+            verifiedUsing: signer.signer
+        ).payload
+
+        try payload.verify(using: signer.signer)
+
+        return payload
+    }
+}
--- Sources/Reset/Provider/ResetProvider.swift
+++ Sources/Reset/Provider/ResetProvider.swift
@@ -14,6 +14,7 @@ public final class ResetProvider<U: JWTAuthenticatable & PasswordResettable> {
 }
 
 // MARK: - Provider
+
 extension ResetProvider: Provider {
     public func register(_ services: inout Services) throws {
         try services.register(MutableLeafTagConfigProvider())
@@ -22,10 +23,6 @@ extension ResetProvider: Provider {
     }
 
     public func didBoot(_ container: Container) throws -> Future<Void> {
-        if config.shouldRegisterRoutes {
-            try registerRoutes(on: container.make())
-        }
-        
         let tags: MutableLeafTagConfig = try container.make()
         tags.use(ResetConfigTag(), as: "reset:config")
 
@@ -34,6 +31,7 @@ extension ResetProvider: Provider {
 }
 
 // MARK: - Commands
+
 extension ResetProvider where U.Database: QuerySupporting, U.ID: LosslessStringConvertible {
     public static func commands(
         databaseIdentifier: DatabaseIdentifier<U.Database>
@@ -47,102 +45,36 @@ extension ResetProvider where U.Database: QuerySupporting, U.ID: LosslessStringC
 }
 
 // MARK: - Routes
-extension ResetProvider {
-    public func renderResetPasswordRequestForm(req: Request) throws -> Future<Response> {
-        return try config.responses.resetPasswordRequestForm(req)
-    }
 
-    public func resetPasswordRequest(req: Request) throws -> Future<Response> {
-        return try U.RequestReset.create(on: req)
-            .flatMap(to: U?.self) { try U.find(by: $0, on: req) }
-            .flatTry { user -> Future<Void> in
-                guard let user = user else {
-                    // ignore case where user could not be found to prevent malicious attackers from
-                    // finding out which accounts are available on the system
-                    return .done(on: req)
-                }
-                return try self.config.reset(
-                    user,
-                    context: U.Context.requestResetPassword(),
-                    on: req
-                )
-            }
-            .flatMap(to: Response.self) { _ in
-                try self.config.responses.resetPasswordEmailSent(req)
-            }
-    }
-
-    public func renderResetPasswordForm(req: Request) throws -> Future<Response> {
-        let payload = try extractVerifiedPayload(from: req.parameters.next())
-
-        return try U
-            .authenticate(using: payload, on: req)
-            .unwrap(or: ResetError.userNotFound)
-            .flatMap(to: Response.self) { user in
-                guard user.passwordChangeCount == payload.passwordChangeCount else {
-                    throw ResetError.tokenAlreadyUsed
-                }
-                return try self.config.responses.resetPasswordForm(req, user)
-        }
-    }
-
-    public func resetPassword(req: Request) throws -> Future<Response> {
-        let payload = try extractVerifiedPayload(from: req.parameters.next())
-
-        return try U
-            .authenticate(using: payload, on: req)
-            .unwrap(or: ResetError.userNotFound)
-            .try { user in
-                guard user.passwordChangeCount == payload.passwordChangeCount else {
-                    throw ResetError.tokenAlreadyUsed
-                }
-            }
-            .flatMap(to: U.self) { user in
-                try U.ResetPassword.create(on: req)
-                    .flatMap(to: U.self) { resetPassword in
-                        var user = user
-                        let password = resetPassword[keyPath: U.ResetPassword.readablePasswordKey]
-                        user[keyPath: U.passwordKey] = try U.hashPassword(password)
-                        user.passwordChangeCount += 1
-                        return user.save(on: req)
-                    }
-            }
-            .flatMap(to: Response.self) { user in
-                try self.config.responses.resetPasswordSuccess(req, user)
-            }
-    }
-}
-
-// MARK: - Private
-private extension ResetProvider {
-    func extractVerifiedPayload(from token: String) throws -> U.JWTPayload {
-        let payload = try JWT<U.JWTPayload>(
-            from: token.convertToData(),
-            verifiedUsing: config.signer.signer
-        ).payload
-
-        try payload.verify(using: config.signer.signer)
-
-        return payload
-    }
-
-    func registerRoutes(on router: Router) {
+public extension Router {
+    public func useResetRoutes<U: JWTAuthenticatable & PasswordResettable>(
+        _ type: U.Type,
+        on container: Container
+    ) throws {
+        let config: ResetConfig<U> = try container.make()
         let endpoints = config.endpoints
+        let controller = config.controller
 
         if let renderResetPasswordRequestPath = endpoints.renderResetPasswordRequest {
-            router.get(renderResetPasswordRequestPath, use: renderResetPasswordRequestForm)
+            get(
+                renderResetPasswordRequestPath,
+                use: controller.renderResetPasswordRequestForm
+            )
         }
 
         if let resetPasswordRequestPath = endpoints.resetPasswordRequest {
-            router.post(resetPasswordRequestPath, use: resetPasswordRequest)
+            post(resetPasswordRequestPath, use: controller.resetPasswordRequest)
         }
 
         if let renderResetPasswordPath = endpoints.renderResetPassword {
-            router.get(renderResetPasswordPath, String.parameter, use: renderResetPasswordForm)
+            get(
+                renderResetPasswordPath, String.parameter,
+                use: controller.renderResetPasswordForm
+            )
         }
 
         if let resetPasswordPath = endpoints.resetPassword {
-            router.post(resetPasswordPath, String.parameter, use: resetPassword)
+            post(resetPasswordPath, String.parameter, use: controller.resetPassword)
         }
     }
 }
