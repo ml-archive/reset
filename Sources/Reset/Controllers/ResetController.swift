396e109476d1939ae56c1e933508e3b0537ad44f
Steffen D. Sommer <steffendsommer@icloud.com>
2018-11-06 13:04:00 +0100

    Fix indentation

--- Sources/Reset/Controllers/ResetController.swift
+++ Sources/Reset/Controllers/ResetController.swift
@@ -42,7 +42,7 @@ public final class ResetController<U: JWTAuthenticatable & PasswordResettable> {
                     throw ResetError.tokenAlreadyUsed
                 }
                 return try config.responses.resetPasswordForm(req, user)
-        }
+            }
     }
 
     public func resetPassword(req: Request) throws -> Future<Response> {
